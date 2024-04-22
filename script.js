32c16fdeb40ede3ca759f966be57c61cb0b5ef84
simonakomarovska <simonak399@gmail.com>
2024-04-22 15:42:36 +0200

    fixed responsive

--- script.js
+++ script.js
@@ -1,44 +1,85 @@
 function validatePassword() {
     let password = document.getElementById("password").value;
-    let errorDisplay = document.getElementById("errorDisplay");
     let isValid = true;
     let result= document.querySelector('#result');
 
     if (password.length < 8) {
         isValid = false;
         result.innerText = "The password must be at least 8 characters or longer";
-        result.classList.add('selected2');
-    } else if (password.length > 28) {
-      isValid = false;
-      result.innerText = "The password cannot be longer than 28 characters";
-      result.classList.add('selected2');
-    } else if (!/[A-Z]/.test(password)) {
-      isValid = false;
-      result.innerText = "Your password must contain at least one capital letter";
-      result.classList.add('selected2');
-    } else if (!/[a-z]/.test(password)) {
-      isValid = false;
-      result.innerText = "Your password must contain at least one lower case letter";
-      result.classList.add('selected2');
-    } else if (!/\d/.test(password)) {
-      isValid = false;
-      result.innerText = "The password must contain at least one number";
-      result.classList.add('selected2');
-    } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-      isValid = false;
-      result.innerText =  "The password must contain at least one special character";
-      result.classList.add('selected2');
-    } else if (isValid) {
-      result.innerText = "Password is Strong!";
-      result.classList.add('selected');
-  }
-
-  document.querySelector('#again').onclick = playAgain;
-  document.querySelector('#again').innerHTML = 'Check Again!';
+        result.classList.add('wrong');
+        result.style.backgroundColor = "#cf7a847f";
+      } else if (password.length > 28) {
+        isValid = false;
+        result.innerText = "The password cannot be longer than 28 characters";
+        result.classList.add('wrong');
+        result.style.backgroundColor = "#cf7a847f";
+      } else if (!/[A-Z]/.test(password)) {
+        isValid = false;
+        result.innerText = "Your password must contain at least one capital letter";
+        result.classList.add('wrong');
+        result.style.backgroundColor = "#cf7a847f";
+      } else if (!/[a-z]/.test(password)) {
+        isValid = false;
+        result.innerText = "Your password must contain at least one lower case letter";
+        result.classList.add('wrong');
+        result.style.backgroundColor = "#cf7a847f";
+      } else if (!/\d/.test(password)) {
+        isValid = false;
+        result.innerText = "The password must contain at least one number";
+        result.classList.add('wrong');
+        result.style.backgroundColor = "#cf7a847f";
+      } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        isValid = false;
+        result.innerText =  "The password must contain at least one special character";
+        result.classList.add('wrong');
+        result.style.backgroundColor = "#cf7a847f";
+      } else if (isValid) {
+        result.innerText = "Password is Strong!";
+        result.classList.add('correct');
+        result.style.backgroundColor = "#367a345b";
+    }
+  document.querySelector('#check').onclick = playcheck;
+  document.querySelector('#check').innerHTML = 'Check again!';
 }
     
-function playAgain() {
-  location.reload();
+function playcheck() {
+  let password = document.getElementById("password").value;
+  let isValid = true;
+  let result= document.querySelector('#result');
 
-  let button =  document.querySelector('#again')
+  if (password.length < 8) {
+    isValid = false;
+    result.innerText = "The password must be at least 8 characters or longer";
+    result.classList.add('wrong');
+    result.style.backgroundColor = "#cf7a847f";
+  } else if (password.length > 28) {
+    isValid = false;
+    result.innerText = "The password cannot be longer than 28 characters";
+    result.classList.add('wrong');
+    result.style.backgroundColor = "#cf7a847f";
+  } else if (!/[A-Z]/.test(password)) {
+    isValid = false;
+    result.innerText = "Your password must contain at least one capital letter";
+    result.classList.add('wrong');
+    result.style.backgroundColor = "#cf7a847f";
+  } else if (!/[a-z]/.test(password)) {
+    isValid = false;
+    result.innerText = "Your password must contain at least one lower case letter";
+    result.classList.add('wrong');
+    result.style.backgroundColor = "#cf7a847f";
+  } else if (!/\d/.test(password)) {
+    isValid = false;
+    result.innerText = "The password must contain at least one number";
+    result.classList.add('wrong');
+    result.style.backgroundColor = "#cf7a847f";
+  } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    isValid = false;
+    result.innerText =  "The password must contain at least one special character";
+    result.classList.add('wrong');
+    result.style.backgroundColor = "#cf7a847f";
+  } else if (isValid) {
+    result.innerText = "Password is Strong!";
+    result.style.backgroundColor = "#367a345b";
+    result.classList.add('correct');
+  }
 }
